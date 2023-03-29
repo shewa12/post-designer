1b125e413e4521411aae123f63d3a8c28460b1d0
shewa12 <shewa12kpi@gmail.com>
2023-03-29 21:35:30 +0600

    Update: Carousel settings implemented on the frontend

--- assets/lib/slick-config.js
+++ assets/lib/slick-config.js
@@ -1,39 +1,80 @@
-jQuery(function($) {
-    $('.pd-slick-slider').slick({
-        dots: true,
-        infinite: true,
-        autoplay: true,
-        speed: 300,
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        arrows: true,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-          // You can unslick at a given breakpoint now by adding:
-          // settings: "unslick"
-          // instead of a settings object
-        ]
-      });
+jQuery(function ($) {
+	var elem = $(".pd-slick-slider");
+
+	if (!elem) {
+		return;
+	}
+
+	var settings = {
+		slidesToShow: Number(elem.attr("data-slides-to-show")),
+		slidesToScroll: Number(elem.attr("data-slides-to-scroll")),
+		initialSlide: Number(elem.attr("data-initial-slide")),
+		dots: Boolean(elem.attr("data-dots")),
+		infinite: Boolean(elem.attr("data-infinite")),
+		autoplay: Boolean(elem.attr("data-autoplay")),
+		arrows: Boolean(elem.attr("data-arrows")),
+		centerMode: Boolean(elem.attr("data-center-mode")),
+		focusOnSelect: Boolean(elem.attr("data-focus-on-select")),
+		rtl: Boolean(elem.attr("data-rtl")),
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					infinite: true,
+					dots: true,
+				},
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+				},
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				},
+			},
+		],
+	};
+
+	console.log(settings);
+	$(".pd-slick-slider").slick(settings);
 });
+		// dots: true,
+		// infinite: true,
+		// autoplay: true,
+		// speed: 300,
+		// slidesToShow: 2,
+		// slidesToScroll: 2,
+		// arrows: true,
+		// responsive: [
+		//   {
+		//     breakpoint: 1024,
+		//     settings: {
+		//       slidesToShow: 3,
+		//       slidesToScroll: 3,
+		//       infinite: true,
+		//       dots: true
+		//     }
+		//   },
+		//   {
+		//     breakpoint: 600,
+		//     settings: {
+		//       slidesToShow: 2,
+		//       slidesToScroll: 2
+		//     }
+		//   },
+		//   {
+		//     breakpoint: 480,
+		//     settings: {
+		//       slidesToShow: 1,
+		//       slidesToScroll: 1
+		//     }
+		//   }
+		// ]
--- templates/post-carousel.php
+++ templates/post-carousel.php
@@ -15,12 +15,13 @@ global $wp_query;
 $args      = Utilities::prepare_args( $attrs );
 $the_query = new WP_Query( $args );
 
+
 if ( $the_query->have_posts() ) :
 	?>
 
 <div class="wp-block-post-designer-carousel">
 
-	<div class='pd-card-row pd-slick-slider'>
+	<div class='pd-card-row pd-slick-slider' <?php echo $attrs['rtl'] ? 'dir="rtl"' : ''; ?> data-slides-to-show="<?php echo esc_attr( $attrs['postPerPage'] ); ?>" data-initial-slide="<?php echo esc_attr( $attrs['initialSlide'] ); ?>" data-dots="<?php echo (bool) esc_attr( $attrs['dots'] ); ?>" data-infinite="<?php echo (bool) esc_attr( $attrs['infinite'] ); ?>" data-autoplay="<?php echo (bool) esc_attr( $attrs['autoplay'] ); ?>" data-center-mode="<?php echo (bool) esc_attr( $attrs['centerMode'] ); ?>" data-speed="<?php echo esc_attr( $attrs['speed'] ); ?>" data-slides-to-scroll="<?php echo esc_attr( $attrs['slidesToScroll'] ); ?>" data-arrows="<?php echo (bool) esc_attr( $attrs['arrows'] ); ?>" data-focus-on-select="<?php echo (bool) esc_attr( $attrs['focusOnSelect'] ); ?>" data-rtl="<?php echo esc_attr( $attrs['rtl'] ? true : false ); ?>">
 		<?php
 		while ( $the_query->have_posts() ) :
 			$the_query->the_post();
