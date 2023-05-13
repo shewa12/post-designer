327f0d84c17d6df7d9067fef28d28c1059f466ac
shewa12 <shewa12kpi@gmail.com>
2023-05-13 18:02:05 +0600

    Update: readme txt, md and main file updated

--- post-designer.php
+++ post-designer.php
@@ -1,14 +1,15 @@
 <?php
 /**
- * Plugin Name:       Post Designer
- * Description:       Design any kind of post as a List & Carousel.
+ * Plugin Name:       Post Designer - Gutenberg post list & carousel blocks
+ * Description:       Post Designer is a WordPress plugin that adds two new Gutenberg blocks: Post List and Post Carousel. The Post List block allows you to display a list of posts, while the Post Carousel block allows you to display a carousel of posts.≈
  * Requires at least: 5.9
  * Requires PHP:      7.0
- * Version:           0.1.0
- * Author:            The WordPress Contributors
- * License:           GPL-2.0-or-later
- * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       blocky
+ * Version:           1.0.0
+ * Author:            Shewa
+ * Author URI:        https://shewazone.com
+ * Text Domain:       post-designer
+ * Domain Path:       /languages
+ * Version:           1.0.0
  *
  * @package           post-designer
  */
@@ -21,128 +22,128 @@ use PostDesigner\Utilities\Utilities;
 
 if ( ! class_exists( 'PostDesigner' ) ) {
 
+/**
+ * PostDesigner main class that trigger the plugin
+ */
+final class PostDesigner {
+
 	/**
-	 * PostDesigner main class that trigger the plugin
+	 * Plugin meta data
+	 *
+	 * @since v1.0.0
+	 *
+	 * @var $plugin_data
 	 */
-	final class PostDesigner {
+	private static $plugin_data = array();
 
-		/**
-		 * Plugin meta data
-		 *
-		 * @since v1.0.0
-		 *
-		 * @var $plugin_data
-		 */
-		private static $plugin_data = array();
+	/**
+	 * Plugin instance
+	 *
+	 * @since v1.0.0
+	 *
+	 * @var $instance
+	 */
+	public static $instance = null;
 
-		/**
-		 * Plugin instance
-		 *
-		 * @since v1.0.0
-		 *
-		 * @var $instance
-		 */
-		public static $instance = null;
-
-		/**
-		 * Register hooks and load dependent files
-		 *
-		 * @since v1.0.0
-		 *
-		 * @return void
-		 */
-		public function __construct() {
-			if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
-				include_once __DIR__ . '/vendor/autoload.php';
-			}
-			register_activation_hook( __FILE__, array( __CLASS__, 'register_activation' ) );
-			register_deactivation_hook( __FILE__, array( __CLASS__, 'register_deactivation' ) );
-			add_action( 'init', array( __CLASS__, 'load_textdomain' ) );
-
-			$this->load_packages();
+	/**
+	 * Register hooks and load dependent files
+	 *
+	 * @since v1.0.0
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+		if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+			include_once __DIR__ . '/vendor/autoload.php';
 		}
+		register_activation_hook( __FILE__, array( __CLASS__, 'register_activation' ) );
+		register_deactivation_hook( __FILE__, array( __CLASS__, 'register_deactivation' ) );
+		add_action( 'init', array( __CLASS__, 'load_textdomain' ) );
 
-		/**
-		 * Plugin meta data
-		 *
-		 * @since v1.0.0
-		 *
-		 * @return array  contains plugin meta data
-		 */
-		public static function plugin_data(): array {
-			if ( ! function_exists( 'get_plugin_data' ) ) {
-				require_once ABSPATH . 'wp-admin/includes/plugin.php';
-			}
-			$plugin_data = get_plugin_data(
-				__FILE__
-			);
-			array_push( self::$plugin_data, $plugin_data );
-
-			self::$plugin_data['plugin_url']  = plugin_dir_url( __FILE__ );
-			self::$plugin_data['plugin_path'] = plugin_dir_path( __FILE__ );
-			self::$plugin_data['base_name']   = plugin_basename( __FILE__ );
-			self::$plugin_data['templates']   = trailingslashit( plugin_dir_path( __FILE__ ) . 'templates' );
-			self::$plugin_data['views']       = trailingslashit( plugin_dir_path( __FILE__ ) . 'views' );
-			self::$plugin_data['assets']      = trailingslashit( plugin_dir_url( __FILE__ ) . 'assets' );
-			self::$plugin_data['base_name']   = plugin_basename( __FILE__ );
-			// set ENV DEV | PROD.
-			self::$plugin_data['env'] = 'DEV';
-			return self::$plugin_data;
-		}
-
-		/**
-		 * Create and return instance of this plugin
-		 *
-		 * @return self  instance of plugin
-		 */
-		public static function instance() {
-			if ( null === self::$instance ) {
-				self::$instance = new self();
-			}
-			return self::$instance;
-		}
-
-		/**
-		 * Do some stuff after activate plugin
-		 *
-		 * @return void
-		 */
-		public static function register_activation() {
-			update_option( '_post_designer_install_time', time() );
-		}
-
-		/**
-		 * Do some stuff after deactivate plugin
-		 *
-		 * @return void
-		 */
-		public static function register_deactivation() {
-
-		}
-
-		/**
-		 * Load plugin text domain
-		 *
-		 * @return void
-		 */
-		public static function load_textdomain() {
-			load_plugin_textdomain( 'post-designer', false, dirname( plugin_basename( __FILE__ ) ) . '/assets/languages' );
-		}
-
-		/**
-		 * Load packages
-		 *
-		 * @return void
-		 */
-		public function load_packages() {
-			new Blocks();
-			new Routes();
-			new Utilities();
-			new Enqueue();
-			new InitAddons();
-		}
+		$this->load_packages();
 	}
-	// trigger.
-	PostDesigner::instance();
+
+	/**
+	 * Plugin meta data
+	 *
+	 * @since v1.0.0
+	 *
+	 * @return array  contains plugin meta data
+	 */
+	public static function plugin_data(): array {
+		if ( ! function_exists( 'get_plugin_data' ) ) {
+			require_once ABSPATH . 'wp-admin/includes/plugin.php';
+		}
+		$plugin_data = get_plugin_data(
+			__FILE__
+		);
+		array_push( self::$plugin_data, $plugin_data );
+
+		self::$plugin_data['plugin_url']  = plugin_dir_url( __FILE__ );
+		self::$plugin_data['plugin_path'] = plugin_dir_path( __FILE__ );
+		self::$plugin_data['base_name']   = plugin_basename( __FILE__ );
+		self::$plugin_data['templates']   = trailingslashit( plugin_dir_path( __FILE__ ) . 'templates' );
+		self::$plugin_data['views']       = trailingslashit( plugin_dir_path( __FILE__ ) . 'views' );
+		self::$plugin_data['assets']      = trailingslashit( plugin_dir_url( __FILE__ ) . 'assets' );
+		self::$plugin_data['base_name']   = plugin_basename( __FILE__ );
+		// set ENV DEV | PROD.
+		self::$plugin_data['env'] = 'DEV';
+		return self::$plugin_data;
+	}
+
+	/**
+	 * Create and return instance of this plugin
+	 *
+	 * @return self  instance of plugin
+	 */
+	public static function instance() {
+		if ( null === self::$instance ) {
+			self::$instance = new self();
+		}
+		return self::$instance;
+	}
+
+	/**
+	 * Do some stuff after activate plugin
+	 *
+	 * @return void
+	 */
+	public static function register_activation() {
+		update_option( '_post_designer_install_time', time() );
+	}
+
+	/**
+	 * Do some stuff after deactivate plugin
+	 *
+	 * @return void
+	 */
+	public static function register_deactivation() {
+
+	}
+
+	/**
+	 * Load plugin text domain
+	 *
+	 * @return void
+	 */
+	public static function load_textdomain() {
+		load_plugin_textdomain( 'post-designer', false, dirname( plugin_basename( __FILE__ ) ) . '/assets/languages' );
+	}
+
+	/**
+	 * Load packages
+	 *
+	 * @return void
+	 */
+	public function load_packages() {
+		new Blocks();
+		new Routes();
+		new Utilities();
+		new Enqueue();
+		new InitAddons();
+	}
+}
+// trigger.
+PostDesigner::instance();
 }
 
--- readme.txt
+++ readme.txt
@@ -1,55 +1,110 @@
-=== Blocky ===
-Contributors:      The WordPress Contributors
-Tags:              block
-Tested up to:      6.0
-Stable tag:        0.1.0
-License:           GPL-2.0-or-later
-License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+=== Post Designer - Gutenberg post list & carousel blocks ===
+Contributors: shewa12
+Donate link: https://www.buymeacoffee.com/shewa
+Tags: WordPress design plugin, Gutenberg blocks, Post Lists, Post Carousels, Post customization, Content carousel, Creative post layouts, Custom post lists, Visual WordPress editor, Enhanced post display, Easy post design, Dynamic blocks
+Requires at least: 5.3
+Tested up to: 6.2
+Requires PHP: 7.0
+Stable tag: 1.0.0
+License: GPLv3
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
 
-Example block scaffolded with Create Block tool.
+=== Description ===
 
-== Description ==
+Post Designer is a WordPress plugin that adds two new Gutenberg blocks: Post List and Post Carousel. The Post List block allows you to display a list of posts, while the Post Carousel block allows you to display a carousel of posts.
 
-This is the long description. No limit, and you can use Markdown (as well as in the following sections).
-
-For backwards compatibility, if this section is missing, the full length of the short description will be used, and
-Markdown parsed.
-
-== Installation ==
-
-This section describes how to install the plugin and get it working.
-
-e.g.
-
-1. Upload the plugin files to the `/wp-content/plugins/blocky` directory, or install the plugin through the WordPress plugins screen directly.
-1. Activate the plugin through the 'Plugins' screen in WordPress
+[youtube https://www.youtube.com/watch?v=_GfXTvSdJTk]
 
 
-== Frequently Asked Questions ==
+=== What can be designed using this plugin? ===
 
-= A question that someone might have =
+&#9989; Design Any Kind of Public Posts
+&#9989; Personal Blog
+&#9989; Magazine
+&#9989; WooCommerce Product
+&#9989; Blog Listing
+&#9989; Blog Carousel
+&#9989; Post Carousel
+&#9989; Post Slider
+&#9989; Content Slider
 
-An answer to that question.
 
-= What about foo bar? =
 
-Answer to foo bar dilemma.
+=== Key Features ===
 
-== Screenshots ==
+&#9989; Post List block
+&#9989; Post Carousel block
+&#9989; Customizable settings for each block
+&#9989; Filters posts by post type, authors, taxonomies
+&#9989; Show-hide each section
+&#9989; Design customization
+&#9989; Carousel settings
+&#9989; Responsive design
+&#9989; Easy to use
+&#9989; Compatible with all major browsers
 
-1. This screen shot description corresponds to screenshot-1.(png|jpg|jpeg|gif). Note that the screenshot is taken from
-the /assets directory or the directory that contains the stable readme.txt (tags or trunk). Screenshots in the /assets
-directory take precedence. For example, `/assets/screenshot-1.png` would win over `/tags/4.3/screenshot-1.png`
-(or jpg, jpeg, gif).
-2. This is the second screen shot
 
-== Changelog ==
+=== Usage ===
 
-= 0.1.0 =
-* Release
+&#9989; To use the Post List block, simply add it to a post or page like any other block. You can then customize the settings for the block, such as the number of posts to display, the order in which to display them, and the layout of the list.
 
-== Arbitrary section ==
+&#9989; To use the Post Carousel block, simply add it to a post or page like any other block. You can then customize the settings for the block, such as the number of posts to display, the order in which to display them, and the layout of the carousel.
 
-You may provide arbitrary sections, in the same format as the ones above. This may be of use for extremely complicated
-plugins where more information needs to be conveyed that doesn't fit into the categories of "description" or
-"installation." Arbitrary sections will be shown below the built-in sections outlined above.
+
+=== Frequently Asked Questions ===
+
+= What is Post Designer =
+
+Answer: Post Designer is a WordPress plugin that allows users to create and customize post lists and carousels with ease. It offers a range of design options and features to enhance the visual presentation of your posts.
+
+= How does Post Designer work? =
+
+Answer: Post Designer integrates seamlessly with the Gutenberg editor in WordPress. It provides custom blocks that enable users to design and style their posts using Post list and carousel blocks.
+
+= Can I customize the appearance of my post lists and carousels? =
+
+Answer: Absolutely! Post Designer offers extensive customization options. You can show-hide each section, change colors, typography, and more. You can filter posts using query filters.
+
+= Is Post Designer compatible with my theme? =
+
+Answer: Post Designer is built to work with most WordPress themes that support the Gutenberg editor. It leverages the native WordPress functionality and follows best practices for compatibility. However, some theme-specific styles and configurations may affect the appearance of your post lists and carousels.
+
+= Can I display post lists and carousels on specific pages or posts only? =
+
+Answer: Yes, you can use it as per your need.
+
+= Does Post Designer affect the performance of my website? =
+
+Answer: Post Designer is designed with performance in mind. It follows optimized coding practices to ensure minimal impact on your website's performance.
+
+= Is support provided for Post Designer? =
+
+Answer: Yes, if you encounter any issues or have questions about the plugin, feel free to ask queries using WordPress plugin support.
+
+= Can I use Post Designer on multiple websites? =
+
+Answer: Yes, it can be used on multiple sites free of cost.
+
+= Is there a demo or documentation available for Post Designer? =
+
+Answer: Yes, 👉 [Learn More](https://www.wpxpo.com/postx/) 🔥
+
+= How often is Post Designer updated? =
+
+Answer: We are committed to keeping Post Designer up to date with the latest WordPress version and ensuring compatibility with new features and improvements. We regularly release updates that include bug fixes, feature enhancements, and security patches.
+
+
+=== Screenshots ===
+
+1. Editor view of the post list block
+2. Frontend view of the post list block
+3. Editor view of the post carousel block
+4. Frontend view of the post carousel block
+
+=== Changelog ===
+
+= 1.0.0 = 
+* Plugin released
+
+
+=== Upgrade Notice ===
