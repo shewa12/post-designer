b210f16bf9909e2b932e719b3b1c73825ecadeaa
shewa12 <shewa12kpi@gmail.com>
2023-05-13 18:04:21 +0600

    Space Indentation: Space & indentation fixed as WPCS

--- post-designer.php
+++ post-designer.php
@@ -22,128 +22,128 @@ use PostDesigner\Utilities\Utilities;
 
 if ( ! class_exists( 'PostDesigner' ) ) {
 
-/**
- * PostDesigner main class that trigger the plugin
- */
-final class PostDesigner {
-
 	/**
-	 * Plugin meta data
-	 *
-	 * @since v1.0.0
-	 *
-	 * @var $plugin_data
+	 * PostDesigner main class that trigger the plugin
 	 */
-	private static $plugin_data = array();
+	final class PostDesigner {
 
-	/**
-	 * Plugin instance
-	 *
-	 * @since v1.0.0
-	 *
-	 * @var $instance
-	 */
-	public static $instance = null;
+		/**
+		 * Plugin meta data
+		 *
+		 * @since v1.0.0
+		 *
+		 * @var $plugin_data
+		 */
+		private static $plugin_data = array();
 
-	/**
-	 * Register hooks and load dependent files
-	 *
-	 * @since v1.0.0
-	 *
-	 * @return void
-	 */
-	public function __construct() {
-		if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
-			include_once __DIR__ . '/vendor/autoload.php';
+		/**
+		 * Plugin instance
+		 *
+		 * @since v1.0.0
+		 *
+		 * @var $instance
+		 */
+		public static $instance = null;
+
+		/**
+		 * Register hooks and load dependent files
+		 *
+		 * @since v1.0.0
+		 *
+		 * @return void
+		 */
+		public function __construct() {
+			if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+				include_once __DIR__ . '/vendor/autoload.php';
+			}
+			register_activation_hook( __FILE__, array( __CLASS__, 'register_activation' ) );
+			register_deactivation_hook( __FILE__, array( __CLASS__, 'register_deactivation' ) );
+			add_action( 'init', array( __CLASS__, 'load_textdomain' ) );
+
+			$this->load_packages();
 		}
-		register_activation_hook( __FILE__, array( __CLASS__, 'register_activation' ) );
-		register_deactivation_hook( __FILE__, array( __CLASS__, 'register_deactivation' ) );
-		add_action( 'init', array( __CLASS__, 'load_textdomain' ) );
 
-		$this->load_packages();
-	}
+		/**
+		 * Plugin meta data
+		 *
+		 * @since v1.0.0
+		 *
+		 * @return array  contains plugin meta data
+		 */
+		public static function plugin_data(): array {
+			if ( ! function_exists( 'get_plugin_data' ) ) {
+				require_once ABSPATH . 'wp-admin/includes/plugin.php';
+			}
+			$plugin_data = get_plugin_data(
+				__FILE__
+			);
+			array_push( self::$plugin_data, $plugin_data );
 
-	/**
-	 * Plugin meta data
-	 *
-	 * @since v1.0.0
-	 *
-	 * @return array  contains plugin meta data
-	 */
-	public static function plugin_data(): array {
-		if ( ! function_exists( 'get_plugin_data' ) ) {
-			require_once ABSPATH . 'wp-admin/includes/plugin.php';
+			self::$plugin_data['plugin_url']  = plugin_dir_url( __FILE__ );
+			self::$plugin_data['plugin_path'] = plugin_dir_path( __FILE__ );
+			self::$plugin_data['base_name']   = plugin_basename( __FILE__ );
+			self::$plugin_data['templates']   = trailingslashit( plugin_dir_path( __FILE__ ) . 'templates' );
+			self::$plugin_data['views']       = trailingslashit( plugin_dir_path( __FILE__ ) . 'views' );
+			self::$plugin_data['assets']      = trailingslashit( plugin_dir_url( __FILE__ ) . 'assets' );
+			self::$plugin_data['base_name']   = plugin_basename( __FILE__ );
+			// set ENV DEV | PROD.
+			self::$plugin_data['env'] = 'DEV';
+			return self::$plugin_data;
 		}
-		$plugin_data = get_plugin_data(
-			__FILE__
-		);
-		array_push( self::$plugin_data, $plugin_data );
 
-		self::$plugin_data['plugin_url']  = plugin_dir_url( __FILE__ );
-		self::$plugin_data['plugin_path'] = plugin_dir_path( __FILE__ );
-		self::$plugin_data['base_name']   = plugin_basename( __FILE__ );
-		self::$plugin_data['templates']   = trailingslashit( plugin_dir_path( __FILE__ ) . 'templates' );
-		self::$plugin_data['views']       = trailingslashit( plugin_dir_path( __FILE__ ) . 'views' );
-		self::$plugin_data['assets']      = trailingslashit( plugin_dir_url( __FILE__ ) . 'assets' );
-		self::$plugin_data['base_name']   = plugin_basename( __FILE__ );
-		// set ENV DEV | PROD.
-		self::$plugin_data['env'] = 'DEV';
-		return self::$plugin_data;
-	}
-
-	/**
-	 * Create and return instance of this plugin
-	 *
-	 * @return self  instance of plugin
-	 */
-	public static function instance() {
-		if ( null === self::$instance ) {
-			self::$instance = new self();
+		/**
+		 * Create and return instance of this plugin
+		 *
+		 * @return self  instance of plugin
+		 */
+		public static function instance() {
+			if ( null === self::$instance ) {
+				self::$instance = new self();
+			}
+			return self::$instance;
 		}
-		return self::$instance;
-	}
 
-	/**
-	 * Do some stuff after activate plugin
-	 *
-	 * @return void
-	 */
-	public static function register_activation() {
-		update_option( '_post_designer_install_time', time() );
-	}
+		/**
+		 * Do some stuff after activate plugin
+		 *
+		 * @return void
+		 */
+		public static function register_activation() {
+			update_option( '_post_designer_install_time', time() );
+		}
 
-	/**
-	 * Do some stuff after deactivate plugin
-	 *
-	 * @return void
-	 */
-	public static function register_deactivation() {
+		/**
+		 * Do some stuff after deactivate plugin
+		 *
+		 * @return void
+		 */
+		public static function register_deactivation() {
 
-	}
+		}
 
-	/**
-	 * Load plugin text domain
-	 *
-	 * @return void
-	 */
-	public static function load_textdomain() {
-		load_plugin_textdomain( 'post-designer', false, dirname( plugin_basename( __FILE__ ) ) . '/assets/languages' );
-	}
+		/**
+		 * Load plugin text domain
+		 *
+		 * @return void
+		 */
+		public static function load_textdomain() {
+			load_plugin_textdomain( 'post-designer', false, dirname( plugin_basename( __FILE__ ) ) . '/assets/languages' );
+		}
 
-	/**
-	 * Load packages
-	 *
-	 * @return void
-	 */
-	public function load_packages() {
-		new Blocks();
-		new Routes();
-		new Utilities();
-		new Enqueue();
-		new InitAddons();
+		/**
+		 * Load packages
+		 *
+		 * @return void
+		 */
+		public function load_packages() {
+			new Blocks();
+			new Routes();
+			new Utilities();
+			new Enqueue();
+			new InitAddons();
+		}
 	}
-}
-// trigger.
-PostDesigner::instance();
+	// trigger.
+	PostDesigner::instance();
 }
 
