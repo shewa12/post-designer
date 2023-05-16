c89d0e243d7f6bfce7ede11760070342220b7f42
shewa12 <shewa12kpi@gmail.com>
2023-05-16 21:12:38 +0600

    Remove: Unwanted symbol removed from plugin desc

--- post-designer.php
+++ post-designer.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name:       Post Designer - Gutenberg post list & carousel blocks
- * Description:       Post Designer is a WordPress plugin that adds two new Gutenberg blocks: Post List and Post Carousel. The Post List block allows you to display a list of posts, while the Post Carousel block allows you to display a carousel of posts.≈
+ * Description:       Post Designer is a WordPress plugin that adds two new Gutenberg blocks: Post List and Post Carousel. The Post List block allows you to display a list of posts, while the Post Carousel block allows you to display a carousel of posts.
  * Requires at least: 5.9
  * Requires PHP:      7.0
  * Version:           1.0.0
